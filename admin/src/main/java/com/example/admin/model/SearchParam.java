a86d8904206a41af05f45bf011f0900f100d4a19
t-g-kim <je23ct@gmail.com>
2021-04-12 00:45:30 +0900

    Set Lombok

--- admin/src/main/java/com/example/admin/model/SearchParam.java
+++ admin/src/main/java/com/example/admin/model/SearchParam.java
@@ -1,33 +1,54 @@
 package com.example.admin.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SearchParam {
     private String account;
     private String email;
     private int page;
 
-    // { "account" : "", "email" : "", "page" : 0}
 
-    public String getAccount() {
-        return account;
-    }
 
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
+//    public SearchParam() {
+//
+//    }
+//
+//    public SearchParam(String account) {
+//        this.account = account;
+//    }
+//
+//    public SearchParam(String account, String email, int page) {
+//        this.account = account;
+//        this.email = email;
+//        this.page = page;
+//    }
+//
+//    // { "account" : "", "email" : "", "page" : 0}
+//
+//    public String getAccount() {
+//        return account;
+//    }
+//
+//    public void setAccount(String account) {
+//        this.account = account;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public int getPage() {
+//        return page;
+//    }
+//
+//    public void setPage(int page) {
+//        this.page = page;
+//    }
 }
